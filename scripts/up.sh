7a5088bea0e39464c7e3d227c21e27edebe4ebe9
louiscklaw <louiscklaw@gmail.com>
2021-11-08 01:42:31 +0800

    update,

--- scripts/up.sh
+++ scripts/up.sh
@@ -2,16 +2,25 @@
 
 set -ex
 
+docker-compose kill || true
+docker-compose down || true
+
 
 docker-compose build
-
-# init laravel
-docker-compose exec php74-httpd "touch /var/www/html/jobbook-laravel/storage/logs/laravel.log"
-docker-compose exec php74-httpd "mkdir -p /var/www/html/jobbook-laravel/storage/logs/"
-docker-compose exec php74-httpd "chmod 777 -R /var/www/html/jobbook-laravel/storage/logs/"
-docker-compose exec php74-httpd "chmod 777 -R /var/www/html/jobbook-laravel/bootstrap/cache"
-docker-compose exec php74-httpd "chmod 777 -R /var/www/html/jobbook-laravel/storage/framework/sessions"
-docker-compose exec php74-httpd "chmod 777 -R /var/www/html/jobbook-laravel/storage/framework/views"
+docker-compose up --no-start
 
 docker-compose up -d
-docker-compose logs -f
+
+sleep 1
+
+# # init laravel
+docker-compose exec -T php74-httpd mkdir -p /var/www/html/jobbook-laravel/storage/logs
+docker-compose exec -T php74-httpd touch /var/www/html/jobbook-laravel/storage/logs/laravel.log
+
+docker-compose exec -T php74-httpd chmod 777 -R /var/www/html/jobbook-laravel/storage/logs/
+docker-compose exec -T php74-httpd chmod 777 -R /var/www/html/jobbook-laravel/bootstrap/cache
+docker-compose exec -T php74-httpd chmod 777 -R /var/www/html/jobbook-laravel/storage/framework/sessions
+docker-compose exec -T php74-httpd chmod 777 -R /var/www/html/jobbook-laravel/storage/framework/views
+
+
+# docker-compose logs -f
